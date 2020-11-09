25d5aba17ae6aab1a69f8ea884e15595361d0936
SkorikRoman <74144359+SkorikRoman@users.noreply.github.com>
2020-11-09 22:32:05 +0600

    Bank

--- app/src/main/java/com/example/mobilebank/MainActivity.java
+++ app/src/main/java/com/example/mobilebank/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.mobilebank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
