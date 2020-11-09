61d20016342fbd7c1dcfec1be0982812f543dd4f
pr318Dmitriev <74090812+pr318Dmitriev@users.noreply.github.com>
2020-11-09 20:11:46 +0600

    Разработка графического интерфейса

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String one = "ssss";
+        String two = "aaaa";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View a = findViewById(R.id.button5);
+
     }
 }
