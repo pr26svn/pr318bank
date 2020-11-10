20aff401ccf0d3c0a81e8eca63af241f5ca07720
Сергей <laym245qwe@mail.ru>
2020-11-10 12:27:40 +0600

    10.11.20

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.TextView;
+
 import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        TextView Date_txt=findViewById(R.id.textView2);
+        Date_txt.setText(format.format(new Date()));
     }
 }
