63b9a7c2504b950f7d31d9ea704c44ba605612ce
GOGR <zairovd340@gmail.com>
2020-11-10 18:22:49 +0600

    Интерфес изменил, добавил свой знак доллара)

--- app/src/main/java/space/dorzhu/test/MainActivity.java
+++ app/src/main/java/space/dorzhu/test/MainActivity.java
@@ -1,21 +1,52 @@
 package space.dorzhu.test;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView mainDate;
     Date date;
+
+        URL url;
+
+    {
+        try {
+            url = new URL("http://www.cbr.ru/scripts/XML_daily.asp?date_req=09/11/2020");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    HttpsURLConnection connection;
+
+    {
+        try {
+            connection = (HttpsURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +56,40 @@ public class MainActivity extends AppCompatActivity {
         String dateText = dateFormat.format(date);
         mainDate = (TextView) findViewById(R.id.data);
         mainDate.setText(dateText);
+        new Process().execute();
     }
 
 
-
-
-    public void onClick(View view){
+    public void onClick(View view) {
 
         Intent intent = new Intent(this, branches.class);
         startActivity(intent);
     }
-    public void onClick2(View view){
+
+    public void onClick2(View view) {
 
         Intent intent = new Intent(this, thirdactivity.class);
         startActivity(intent);
     }
 
+
 }
+
+
+class Process extends AsyncTask{
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
