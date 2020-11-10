070acbb7cdd9dff264272025c05252589bf9f1d9
GOGR <zairovd340@gmail.com>
2020-11-10 12:17:34 +0600

    Интерфес изменил, добавил свой знак доллара)

--- app/src/main/java/space/dorzhu/test/MainActivity.java
+++ app/src/main/java/space/dorzhu/test/MainActivity.java
@@ -1,14 +1,44 @@
 package space.dorzhu.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mainDate;
+    Date date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        String dateText = dateFormat.format(date);
+        mainDate = (TextView) findViewById(R.id.data);
+        mainDate.setText(dateText);
     }
+
+
+
+
+    public void onClick(View view){
+
+        Intent intent = new Intent(this, branches.class);
+        startActivity(intent);
+    }
+    public void onClick2(View view){
+
+        Intent intent = new Intent(this, thirdactivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/space/dorzhu/test/secondpage.java
@@ -0,0 +1,13 @@
+package space.dorzhu.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class secondpage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
