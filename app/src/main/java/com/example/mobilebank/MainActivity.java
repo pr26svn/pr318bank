4e4715590536f91db28340c2455b67754ee4f562
SkorikRoman <74144359+SkorikRoman@users.noreply.github.com>
2020-11-10 12:22:37 +0600

    Bank

--- app/src/main/java/com/example/mobilebank/MainActivity.java
+++ app/src/main/java/com/example/mobilebank/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.mobilebank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Date date;
+    TextView mainDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        date = new Date();
+        DateFormat dateFormat=new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        String dataText=dateFormat.format(date);
+        mainDate=(TextView) findViewById(R.id.data);
+        mainDate.setText(dataText);
+    }
+
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, Branches.class);
+        startActivity(intent);
     }
 }
