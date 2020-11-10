16c3be1902746b24fa7c5533366f73d8ad452d29
idaniil24 <i_daniil24@mail.ru>
2020-11-10 22:32:41 +0600

    добавление второго экрана и его итерфейса, добавление функционала кнопки отделения и банкоматы

--- app/src/main/java/com/example/pr318ignatev/SecondActivity.java
+++ app/src/main/java/com/example/pr318ignatev/SecondActivity.java
@@ -5,19 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener(){
+        setContentView(R.layout.activity_second);
+        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.layout1);
+        linearLayout1.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
