1a93afb3519671deb9824ac3f51e1eee07a4362e
Pr318LychkovEruslan <luchkov37fm@gmail.com>
2020-11-10 23:35:48 +0600

    Создание первого экрана, не доделал потому что появилась ошибка(Error while waiting for device: The emulator process for AVD Pixel_2_API_30 was killed.)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
