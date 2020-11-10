9e90d025238e7742281316ed7f1f8628cf0a9315
xSpxrk <krut.slava@inbox.ru>
2020-11-10 08:57:22 +0600

    Исправил интерфейс и сделал переход на другое окно

--- Bank/src/main/java/com/example/myapplication/MainActivity.java
+++ Bank/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, Atms.class);
+        startActivity(intent);
     }
 }
