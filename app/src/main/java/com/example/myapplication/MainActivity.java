327a988a49e1e83569ef9fda4848332b6dde4839
pr318poddubnyy <poddubnyy_2002@mail.ru>
2020-11-10 12:09:01 +0600

    Законченный интерфейс, сделанны переключения между activity, дата берется автоматически с учетом формата dd.MM.yyyy. Чуть позже приступлю к диалоговому окну авторизации и экрану "отделения и банкоматы"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Date date;
+    TextView mainDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        String dateText = dateFormat.format(date);
+        mainDate=(TextView) findViewById(R.id.textView5);
+        mainDate.setText(dateText);
+    }
+
+
+    public void OnClickBankomats(View view) {
+        Intent intent = new Intent(this, secondActivity.class);
+        startActivity(intent);
+    }
+
+    public void OnClickCurrency(View view) {
+        Intent intent = new Intent(this, firstActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/firstActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstacrivity);
+    }
+}
