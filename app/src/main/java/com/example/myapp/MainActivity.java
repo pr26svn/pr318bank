0aaca1e5ff471e804110320303212f2d6d5a5ea5
pr-318KaymasidiMatvey <74090261+pr-318KaymasidiMatvey@users.noreply.github.com>
2020-11-10 14:24:51 +0600

    10.11.2020

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Date date;
+    TextView mainDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy", Locale.getDefault());
+        String dateText = dateFormat.format(date);
+        mainDate = (TextView) findViewById(R.id.date);
+        mainDate.setText(dateText);
     }
+
 }
