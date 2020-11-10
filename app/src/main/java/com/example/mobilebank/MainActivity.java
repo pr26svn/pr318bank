a18dead6e7e0188cbc193229cb3009fb6e809497
stasr <stas.radchenko.den@mail.ru>
2020-11-10 08:33:32 +0600

    Добавляю функционал кнопкам на главном экране

--- app/src/main/java/com/example/mobilebank/MainActivity.java
+++ app/src/main/java/com/example/mobilebank/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.mobilebank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout btn_otdelenia, btn_currency;
+    Button btn_enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_otdelenia = (LinearLayout) findViewById(R.id.btn_otdelenia);
+        btn_currency = (LinearLayout) findViewById(R.id.btn_currency);
+        btn_enter = (Button) findViewById(R.id.btn_enter);
+
+        btn_otdelenia.setOnClickListener(this);
+        btn_currency.setOnClickListener(this);
+        btn_enter.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btn_otdelenia:
+                 intent = new Intent(this, Otdelenia.class);
+                 startActivity(intent);
+                 break;
+            case R.id.btn_currency:
+                 Intent intent2 = new Intent(this, Currency.class);
+                 startActivity(intent2);
+                 break;
+            case R.id.btn_enter:
+                Toast.makeText( this, "Вход...", Toast.LENGTH_LONG ).show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
---
+++ app/src/main/java/com/example/mobilebank/Otdelenia.java
@@ -0,0 +1,31 @@
+package com.example.mobilebank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Otdelenia extends AppCompatActivity {
+
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otdelenia);
+
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+    }
+}
