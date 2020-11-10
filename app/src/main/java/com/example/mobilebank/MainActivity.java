95f69bee9d37107e6b0349c33afe0270077ade83
stasr <stas.radchenko.den@mail.ru>
2020-11-10 18:13:02 +0600

    Сделал "Авторизацию", мелкие исправления

--- app/src/main/java/com/example/mobilebank/MainActivity.java
+++ app/src/main/java/com/example/mobilebank/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.mobilebank;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +20,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    final Context context = this;
     LinearLayout btn_otdelenia, btn_currency;
     Button btn_enter;
 
@@ -41,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_otdelenia.setOnClickListener(this);
         btn_currency.setOnClickListener(this);
         btn_enter.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -56,10 +64,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                  startActivity(intent2);
                  break;
             case R.id.btn_enter:
-
+                createDialog();
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
         }
+    }
+
+    void createDialog() {
+        //Получаем вид с файла prompt.xml, который применим для диалогового окна:
+        LayoutInflater li = LayoutInflater.from(context);
+        View registrationView = li.inflate(R.layout.registration, null);
+
+
+        //Создаем AlertDialog
+        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+
+        //Настраиваем registration.xml для нашего AlertDialog:
+        mDialogBuilder.setView(registrationView);
+
+        //Настраиваем отображение поля для ввода текста в открытом диалоге:
+        final EditText userInputLogin = (EditText) registrationView.findViewById(R.id.input_login);
+        final EditText userInputPassword = (EditText) registrationView.findViewById(R.id.input_password);
+
+
+        //Настраиваем сообщение в диалоговом окне:
+        mDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+
+            }
+        }).setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                dialog.cancel();
+            }
+        });
+
+        //Создаем AlertDialog:
+        AlertDialog alertDialog = mDialogBuilder.create();
+
+        //и отображаем его:
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/example/mobilebank/Otdelenia.java
+++ app/src/main/java/com/example/mobilebank/Otdelenia.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Otdelenia extends Activity {
+public class Otdelenia extends AppCompatActivity implements View.OnClickListener {
 
+    String toast_result;
+
+    ImageButton btn_back;
 
     ArrayList<Otdelenie> otdelenies = new ArrayList<Otdelenie>();
     BoxAdapter boxAdapter;
@@ -23,6 +34,8 @@ public class Otdelenia extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otdelenia);
 
+        btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(this);
 
         // создаем адаптер
         fillData();
@@ -31,14 +44,40 @@ public class Otdelenia extends Activity {
         // настраиваем список
         ListView lvMain = (ListView) findViewById(R.id.lvMain);
         lvMain.setAdapter(boxAdapter);
+
+        otdelenies.add(new Otdelenie("г. Омск", "08:00-20:00", "Не работает", "Отделение"));
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_for_otdelenia,
+                        (ViewGroup) findViewById(R.id.custom_toast_container));
+
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+
+
     }
 
     // генерируем данные для адаптера
     void fillData() {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 19; i++) {
             otdelenies.add(otd);
         }
     }
 
 
+
+    @Override
+    public void onClick(View v) {
+        finish();
+    }
 }
