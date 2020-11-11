0e2f1d81a530b0829f3481e8d9c773a239e72c44
xSpxrk <krut.slava@inbox.ru>
2020-11-11 12:02:08 +0600

    Доделал диалоговое окно

--- Bank/src/main/java/com/example/myapplication/ExampleDialog.java
+++ Bank/src/main/java/com/example/myapplication/ExampleDialog.java
@@ -15,10 +15,22 @@ public class ExampleDialog extends AppCompatDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.Theme_AppCompat_Light_Dialog_Alert);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.Test);
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.layout_login_fragment, null);
-        builder.setView(view);
+        builder.setView(view)
+                .setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
                 return builder.create();
 
 
