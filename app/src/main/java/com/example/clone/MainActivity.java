53f07d51f83eca0ac7b4ac87eb16322a56706a15
pr318gryzlov <fordd19_96@mail.ru>
2020-11-11 11:55:38 +0600

    Сделал AlertDialog, далее RestApi

--- app/src/main/java/com/example/clone/MainActivity.java
+++ app/src/main/java/com/example/clone/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.clone;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -38,6 +40,55 @@ public class MainActivity extends AppCompatActivity {
     public void onClickCurancy(View view) {
         intent = new Intent(this, ThirthActivity.class);
         startActivity(intent);
+    }
+    public void showAlertDialogButtonClicked(View view)
+    {
+
+        // Create an alert builder
+        AlertDialog.Builder builder
+                = new AlertDialog.Builder(this, R.style.mainLogin);
+
+        // set the custom layout
+        final View customLayout
+                = getLayoutInflater()
+                .inflate(
+                        R.layout.custom_layout,
+                        null);
+        builder.setView(customLayout);
+
+        // add a button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                EditText username = customLayout.findViewById(R.id.mainUsername);
+                sendDialogDataToActivity(username.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText password = customLayout.findViewById(R.id.mainPassword);
+                sendDialogDataToActivity(password.getText().toString());
+            }
+        });
+
+        // create and show
+        // the alert dialog
+        AlertDialog dialog
+            = builder.create();
+        dialog.show();
+    }
+
+    // Do something with the data
+    // coming from the AlertDialog
+    private void sendDialogDataToActivity(String data)
+    {
+        Toast.makeText(this,
+                data,
+                Toast.LENGTH_SHORT)
+                .show();
     }
 
 
