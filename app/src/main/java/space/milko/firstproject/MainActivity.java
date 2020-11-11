e9d1b554cff5335912ec64fe99836369d8da6369
pr318milkoivan <ivan.milko@yandex.ru>
2020-11-11 11:20:34 +0600

    Редактирование 2-го экрана

--- app/src/main/java/space/milko/firstproject/MainActivity.java
+++ app/src/main/java/space/milko/firstproject/MainActivity.java
@@ -11,7 +11,16 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -40,17 +49,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-//    AsyncTask.execute(new Runnable() {
-//        @Override
-//        public void run() {
-//            URL generatedUrl = new URL("http://www.cbr.ru/scripts/XML_daily.asp?date_req=10/11/2020");
-//            HttpURLConnection connectionForCourse = (HttpURLConnection) generatedUrl.openConnection();
-//            connectionForCourse.setRequestProperty("User-Agent", "bank");
-//            if(connectionForCourse.getResponseCode()==200){
+//        DownloadData downloadData = new DownloadData();
+//        downloadData.execute("http://www.cbr.ru/scripts/XML_daily.asp?date_req=10/11/2020");
+
+//        AsyncTask.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                URL generatedUrl = null;
+//                try {
+//                    generatedUrl = new URL("http://www.cbr.ru/scripts/XML_daily.asp?date_req=10/11/2020");
+//
+//                    HttpURLConnection connectionForCourse = (HttpURLConnection) generatedUrl.openConnection();
+//                    connectionForCourse.setRequestProperty("User-Agent", "bank");
+//                    if(connectionForCourse.getResponseCode()==200){
+//                        InputStream responseBody = connectionForCourse.getInputStream();
+//                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+//                        xmlReader = new XmlPullParser (responseBodyReader);
+//
+//                    }else{
+//
+//                    }
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
 //
 //            }
-//
-//        }
-//    });
-}
+//        });
+    }
+
+
+
+
+    }
