774b78c2feb3b6a3761bd0366b38991f92dcc76c
User <nastyushka20020711@gmail.com>
2020-11-12 11:56:46 +0600

    12.11.20

--- app/src/main/java/com/example/mobileBank/AtmActivity.java
+++ app/src/main/java/com/example/mobileBank/AtmActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AtmActivity extends AppCompatActivity {
 
@@ -13,6 +16,28 @@ public class AtmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atm);
+
+        ListView listView;
+        listView = findViewById(R.id.listViewAtms);
+        ArrayList<Banks> arrayList = new ArrayList<>();
+
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+
+        BanksAdapter banksAdapter = new BanksAdapter(this, R.layout.list_row_atm, arrayList);
+
+        listView.setAdapter(banksAdapter);
+
     }
 
     public void returnBack_Click(View view) {
---
+++ app/src/main/java/com/example/mobileBank/BanksAdapter.java
@@ -0,0 +1,55 @@
+package com.example.mobileBank;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class BanksAdapter extends ArrayAdapter<Banks> {
+
+    private Context mContext;
+    private int mResourse;
+
+
+    public BanksAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Banks> objects) {
+        super(context, resource, objects);
+
+        this.mContext=context;
+        this.mResourse=resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+
+        convertView=layoutInflater.inflate(mResourse,parent,false);
+
+        TextView streets = convertView.findViewById(R.id.txtAddress);
+
+        TextView atms = convertView.findViewById(R.id.txtAtms);
+
+        TextView jobs = convertView.findViewById(R.id.txtJobs);
+
+        TextView hours = convertView.findViewById(R.id.txtHours);
+
+        streets.setText(getItem(position).getStreets());
+
+        atms.setText(getItem(position).getAtm());
+
+        jobs.setText(getItem(position).getJobs());
+
+        hours.setText(getItem(position).getHours());
+
+
+        return convertView;
+    }
+}
+
