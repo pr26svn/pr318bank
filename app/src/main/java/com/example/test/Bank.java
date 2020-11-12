e4afbc59f1912369589b748b82e9a98e685eeb16
MargaritaMulkeeva <mulkeevarita2001@gmail.com>
2020-11-12 11:54:52 +0600

    12.11.2020

--- app/src/main/java/com/example/test/Bank.java
+++ app/src/main/java/com/example/test/Bank.java
@@ -6,6 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Bank extends AppCompatActivity {
 
@@ -14,7 +22,7 @@ public class Bank extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank);
 
-        Button button3 = (Button) findViewById(R.id.button9);
+        Button button3 = (Button) findViewById(R.id.button8);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +30,65 @@ public class Bank extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ListView listView;
+        listView = findViewById(R.id.listView);
+        ArrayList<Banks> arrayList = new ArrayList<>();
+
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new Banks( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+
+        BanksAdapter banksAdapter = new BanksAdapter(this, R.layout.list, arrayList);
+
+        listView.setAdapter(banksAdapter);
+
+    }
+
+    private void downloandData (){
+        String query= "https://api.privatbank.ua/p24api/infrastructure?json&atm&address=&city=";
+        HttpsURLConnection connection = null;
+        try{
+            connection = (HttpsURLConnection)new URL(query).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection  .setConnectTimeout(250);
+            connection.setReadTimeout(250);
+
+            connection.connect();
+
+            StringBuilder sb= new StringBuilder();
+
+            if(HttpsURLConnection.HTTP_OK==connection.getResponseCode()){
+                BufferedReader in= new BufferedReader(new InputStreamReader(connection.getInputStream(), "cp1251"));
+
+                String line;
+                while((line=in.readLine())!=null){
+                    sb.append(line);
+                    sb.append('\n');
+                }
+                System.out.println(sb.toString());
+            } else {
+                System.out.println("fail"+connection.getResponseCode()+ ", "+ connection.getResponseMessage());
+            }
+
+        }
+        catch (Throwable cause){
+            cause.printStackTrace();
+        } finally {
+            if(connection!=null){
+                connection.disconnect();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -162,28 +162,13 @@ public class MainActivity extends AppCompatActivity {
 
         }*/
 
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Bank.class);
-                startActivity(intent);
-            }
-        });
-        Button button1 = (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Valute.class);
-                startActivity(intent);
-            }
-        });
+
         Button button2 = (Button) findViewById(R.id.button);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final EditText editText = findViewById(R.id.Et);
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.builder);
                 LayoutInflater inflater = MainActivity.this.getLayoutInflater();
                 builder.setView(inflater.inflate(R.layout.activity_log_in, null));
                 builder.setCancelable(false);
@@ -211,6 +196,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void ATM(View view) {
+        Intent intent = new Intent(MainActivity.this, Bank.class);
+        startActivity(intent);    }
+
+    public void valute(View view) {
+        Intent intent = new Intent(MainActivity.this, Valute.class);
+        startActivity(intent);
+    }
 }
 
 
