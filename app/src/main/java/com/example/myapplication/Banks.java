715fc0c1ba22d1194f07473aaeb3ff87b742bfc6
Сергей <laym245qwe@mail.ru>
2020-11-12 12:01:13 +0600

    12.11.20

--- app/src/main/java/com/example/myapplication/Banks.java
+++ app/src/main/java/com/example/myapplication/Banks.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Banks extends AppCompatActivity {
 
@@ -23,5 +26,26 @@ public class Banks extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ListView listView;
+        listView = findViewById(R.id.listView);
+        ArrayList<bba> arrayList = new ArrayList<>();
+
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "00:00-00:00" ));
+        arrayList.add(new bba( "Москва", "Бонкомат", "Работает", "09:00-20:00" ));
+
+        BanksAdapter banksAdapter = new BanksAdapter(this,R.layout.vvv,arrayList);
+
+        listView.setAdapter(banksAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/bba.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+public class bba {
+    String streets;
+    String atm;
+    String jobs;
+    String hours;
+
+    public bba(String streets, String atm, String jobs, String hours) {
+        this.streets = streets;
+        this.atm = atm;
+        this.jobs = jobs;
+        this.hours = hours;
+    }
+
+    public String getStreets() {
+        return streets;
+    }
+
+    public void setStreets(String streets) {
+        this.streets = streets;
+    }
+
+    public String getAtm() {
+        return atm;
+    }
+
+    public void setAtm(String atm) {
+        this.atm = atm;
+    }
+
+    public String getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(String jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+}
+
