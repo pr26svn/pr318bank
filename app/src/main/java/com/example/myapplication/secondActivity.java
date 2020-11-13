dd8999da4f70dd753ea556ffba49cb7ca19310d3
pr318poddubnyy <poddubnyy_2002@mail.ru>
2020-11-13 20:26:10 +0600

    Окончательно завершённый дизайн окна с выводом курса валют и законченный парсинг с сайта. Осталось сделать вывод курса EUR и USD, а также доделать дизайн окна с банкоматами и сделать их парсинг с сайта

--- app/src/main/java/com/example/myapplication/secondActivity.java
+++ app/src/main/java/com/example/myapplication/secondActivity.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.DateFormat;
@@ -36,6 +43,8 @@ public class secondActivity extends AppCompatActivity {
         mainDate.setText(dateText);
 
         listView=(ListView)findViewById(R.id.listView1);
+        new newThread().execute();
+        currencyAdapter = new CurrencyAdapter(this, R.layout.list_style, currencyArrayList);
 /*        String blokXml="";
         txtXml = (TextView) findViewById(R.id.textView13);
         XmlPullParser xpp = getResources().getXml(R.xml.currency);
@@ -49,7 +58,45 @@ public class secondActivity extends AppCompatActivity {
         }*/
     }
 
+    public class newThread extends AsyncTask<String,  Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            Document doc;
+            int flg = 0;
+            try{
+                doc = Jsoup.connect("https://www.profinance.ru/currency_usd.asp%22").get();
+                        Elements elements = doc.select("tr.stat");
+                currencyArrayList.clear();
 
+
+                for(Element element : elements){
+                    if(flg!= 0){
+                        String title = element.child(0).text();
+                        String name = element.child(2).text();
+                        String value = element.child(3).text();
+
+
+                        currencyArrayList.add(new Currency(name,
+                                title, value, R.drawable.usa));
+                    } else{
+                        flg = 1;
+                    }
+
+                }
+
+
+
+            }catch ( IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            listView.setAdapter(currencyAdapter);
+        }
+    }
 
     public void finish(View view) {
         finish();
