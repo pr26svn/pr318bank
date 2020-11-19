2ba46335f67d650820a1cd3631fb47a0ba4fbbd1
Igor <eternal.ded@gmail.com>
2020-11-19 20:04:49 +0600

    Исправления дизайна.

--- app/src/main/java/com/example/bank/login_activity.java
+++ app/src/main/java/com/example/bank/login_activity.java
@@ -1,7 +1,6 @@
 package com.example.bank;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +11,5 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
-    }
-
-    public void onCancel(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
